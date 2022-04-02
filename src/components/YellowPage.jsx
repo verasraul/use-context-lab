12a7429a60a6c27cd8fc45d0612c12748a6f5c56
Raul Veras <verasraul@gmail.com>
2022-04-02 13:44:12 -0400

    added useContext to YellowPage component

--- src/components/YellowPage.jsx
+++ src/components/YellowPage.jsx
@@ -1,6 +1,10 @@
-// import React from "react";
+// import React from "
+import React, {useContext} from 'react';
+import { ColorContext } from '../App';
 
 function YellowPage(){
+    const Context = useContext(ColorContext);
+    console.log(Context);
     return(
         <div>
             <h1 style={{color: 'yellow'}}>This is the YellowPage</h1>
